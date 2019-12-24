4578a66676e185d61057a001a7a433ce91fd622d
Josh Gubler <josh.gubler@gmail.com>
2019-12-24 10:28:32 -0700

    add module block to example

--- examples/example.tf
+++ examples/example.tf
@@ -3,3 +3,7 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+module "" {
+  source = "git@github.com:byu-oit/terraform-aws-<module_name>?ref=v1.0.0"
+  #source = "../" # for local testing during module development
+}
