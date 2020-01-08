bce195e0c1deca8b276d4035760fea54d356c565
William Wallace <willtwall@googlemail.com>
2020-01-07 17:35:20 -0700

    Started some work on the main, variables, and outputs terraform files.

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ terraform {
   }
 }
 
+module "terraform-aws-rds" {
+  source = "git@github.com:byu-oit/terrafrom-aws-rds"
+
+  resource "aws_db_instance" "database" {
+    username = var.username
+    password = var.password
+    identifier = var.instance_name
+    engine = var.db_engine
+    
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "username" {
+    type        = string
+    description = "Master username for the database instance."
+}
+
+variable "password" {
+    type = string
+    description = "Password for the master username"
+}
+
+variable "instance_name" {
+    type = string
+    description = "Name for the DB Instance"
+}
+
+variable "db_engine" {
+    type = string
+    description = "The engine to be used for the database"
+}
