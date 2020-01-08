303bfba106b1d466dc36a571f2885b1202c0e6fc
William Wallace <willtwall@googlemail.com>
2020-01-07 17:46:11 -0700

    Quick change for testing.

--- main.tf
+++ main.tf
@@ -6,14 +6,15 @@ terraform {
 }
 
 module "terraform-aws-rds" {
-  source = "git@github.com:byu-oit/terrafrom-aws-rds"
+  source = "https://gitlab.com/masterqwerty/terraform-aws-rds"
 
   resource "aws_db_instance" "database" {
-    username = var.username
-    password = var.password
-    identifier = var.instance_name
-    engine = var.db_engine
-    
+    username       = var.username
+    password       = var.password
+    identifier     = var.instance_name
+    engine         = var.db_engine
+    instance_class = var.instance_type
+
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,19 +1,24 @@
 variable "username" {
-    type        = string
-    description = "Master username for the database instance."
+  type        = string
+  description = "Master username for the database instance."
 }
 
 variable "password" {
-    type = string
-    description = "Password for the master username"
+  type        = string
+  description = "Password for the master username"
 }
 
 variable "instance_name" {
-    type = string
-    description = "Name for the DB Instance"
+  type        = string
+  description = "Name for the DB Instance"
 }
 
 variable "db_engine" {
-    type = string
-    description = "The engine to be used for the database"
+  type        = string
+  description = "The engine to be used for the database"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The instance type to use for the database"
 }
