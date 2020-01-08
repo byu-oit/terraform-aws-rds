94411d2c5b882083aa274da3f85b0120bd7d0a01
William Wallace <willtwall@googlemail.com>
2020-01-08 08:02:57 -0700

    Added allocated_storage to main.tf.

--- main.tf
+++ main.tf
@@ -6,11 +6,12 @@ terraform {
 }
 
 resource "aws_db_instance" "database" {
-  username       = var.db_username
-  password       = var.db_password
-  identifier     = var.instance_name
-  engine         = var.db_engine
-  instance_class = var.instance_class
-  name           = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+  identifier        = var.instance_name
+  engine            = var.db_engine
+  instance_class    = var.instance_class
+  name              = var.db_name
+  allocated_storage = var.db_storage
 }
 
