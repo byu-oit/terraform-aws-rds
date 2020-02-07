f96ae1b9f694659f44f5c9dcc50c20b97c93ffec
William Wallace <willtwall@googlemail.com>
2020-02-07 14:15:50 -0700

    Added a version change to the example and README.

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -9,7 +9,7 @@ module "acs" {
 }
 
 module "rds" {
-  //  source = "github.com/byu-oit/terraform-aws-rds?ref=v0.2.0"
+  //  source = "github.com/byu-oit/terraform-aws-rds?ref=v0.2.1"
   source                  = "../.."
   identifier              = "example"
   engine                  = "mysql"
