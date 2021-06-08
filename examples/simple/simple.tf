be3be5f169bf2277a0b068bfdf7da12f9ab12886
Spencer Tuft <stuft2@byu.edu>
2021-06-08 13:07:03 -0600

    Update examples & README

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -8,7 +8,7 @@ module "acs" {
 }
 
 module "rds" {
-  source = "github.com/byu-oit/terraform-aws-rds?ref=v2.3.1"
+  source = "github.com/byu-oit/terraform-aws-rds?ref=v2.3.2"
   //  source                  = "../.."
   identifier              = "example"
   engine                  = "mysql"
